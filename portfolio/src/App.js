5ba27fc926c9495e471455b3716ba95b67c11a68
BarackAdida <adida.barack@student.moringaschool.com>
2026-06-17 08:28:37 +0300

    Updated body and header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Header from './components/Header';
+import Body from './components/Body';
 
 const App = () => {
   return (
     <div id="app" className="App">
         Hello World
+        <Header />
+        <Body />
     </div>
   );
 }
